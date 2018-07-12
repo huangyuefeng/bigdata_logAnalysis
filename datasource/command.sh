c1bb425f1858aee42c0e1bd58820df080f290ce9
huangyuefeng <huangyuefeng1@wanda.cn>
2018-07-12 11:47:00 +0800

    02

--- datasource/command.sh
+++ datasource/command.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# Update by huangyuefeng On 2018/07/12
 
-/home/bigdata/datasource/user.py 1000
-/home/bigdata/datasource/brand.py 1000
-/home/bigdata/datasource/record.py 100000
+
+/home/zkpk/datasource/user.py 1000
+/home/zkpk/datasource/brand.py 1000
+/home/zkpk/datasource/record.py 100000
